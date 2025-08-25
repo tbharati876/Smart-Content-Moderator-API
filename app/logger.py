a788665d4a3a0d2ad4f043a5d00dad3b49938152
tbharati876 <tbharati583@gmail.com>
2025-08-25 20:47:28 +0530

    logger.py

---
+++ app/logger.py
@@ -0,0 +1,8 @@
+# app/logger.py
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+)
+logger = logging.getLogger("moderator")
